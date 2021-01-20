99225ae7e0b2efe79e56816a6bceb4c4c6a636a3
Luizzmy <luisg.valenzuelam@gmail.com>
2021-01-20 11:03:06 +0200

    controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+
+  get 'daily_logs/index'
+  get 'daily_logs/show'
+  get 'daily_logs/new'
   devise_for :users
   root 'frontpage#index'
+
+  resources :activities do 
+    member do 
+      get :delete
+    end
+  end
+
+  resources :daily_logs do 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
