b6bf79017d8e3cbe142608e7f38b9145bc46ca99
Luizzmy <luisg.valenzuelam@gmail.com>
2021-01-20 15:51:47 +0200

    basic CRUD done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,6 +5,7 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
+    @daily_logs = DailyLog.where(:activity_id => params[:id])
   end
 
   def new
@@ -14,6 +15,7 @@ class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(activity_params)
     if @activity.save
+      flash[:notice] ="Activity created succesfully"
       redirect_to(activities_path)
     else
       render('new')
@@ -27,6 +29,7 @@ class ActivitiesController < ApplicationController
   def update
     @activity = Activity.find(params[:id])
     if @activity.update(activity_params)
+      flash[:notice] ="Activity updated succesfully"
       redirect_to(activity_path(@activity))
     else
       render('edit')
@@ -40,6 +43,7 @@ class ActivitiesController < ApplicationController
   def destroy
     @activity = Activity.find(params[:id])
     @activity.destroy
+    flash[:notice] ="Activity '#{@activity.name}' destroyed succesfully"
     redirect_to(activities_path)
   end
 
--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -6,9 +6,20 @@ class DailyLogsController < ApplicationController
   end
 
   def new
+    @daily_log= DailyLog.new(params[:activity_id])
   end
 
   def create
+    @daily_log=DailyLog.new(daily_params)
+    if @daily_log.save
+      flash[:notice]="Daily Log added succesfully"
+      redirect_to(activities_path)
+    else
+      render('new')
+    end
   end
 
+  def daily_params
+    params.require(:daily_log).permit(:activity_id, :achieved, :comments)
+  end
 end
